13a8b6ef7de099a0d0e74d1f68d3a72904f8376e
mohamed-2112 <mohamed.rasmy.fathy@gmail.com>
2024-05-26 17:04:39 -0400

    refactoring the constants file

--- main/applications_runner.py
+++ main/applications_runner.py
@@ -1,16 +1,15 @@
 from main.applications_with_langchain.application_interface import Application
-from main.constants.informations import Informations
-
+from main.constants.constant import INFORMATION, PROFILE_URL
 
 
 class ApplicationRunner:
-    def __init__(self , application: Application):
+    def __init__(self, application: Application):
         self.application = application
-    
+
     def langchainTestApp(self):
-        information = Informations.information_1
-        self.application.run(information = information)
+        information = INFORMATION
+        self.application.run(information=information)
 
     def ice_breaker_app(self):
-        profile_url = 'https://linkedin.com/in/johnrmarty/'
+        profile_url = PROFILE_URL
         self.application.run(profile_url=profile_url)
